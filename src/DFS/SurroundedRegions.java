da0076b9c3f6a245f4fae59a832ccc18151e70a7
codzeroNov <codzero@qq.com>
2020-07-31 16:28:45 +0800

    Surrounded Regions

---
+++ src/DFS/SurroundedRegions.java
@@ -0,0 +1,188 @@
+package DFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class SurroundedRegions {
+    /*
+        Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+        A region is captured by flipping all 'O's into 'X's in that surrounded region.
+    */
+    //recursive way
+    int m, n;
+
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0)
+            return;
+        m = board.length;
+        n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isEdge = (i == 0 || i == m - 1 || j == 0 || j == n - 1);
+                if (board[i][j] == 'O' && isEdge)
+                    dfs(board, i, j);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == 'X' || board[i][j] == '#')
+            return;
+
+        board[i][j] = '#';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+
+    //dfs using stack
+
+    private class Pos {
+        int i;
+        int j;
+
+        Pos(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    public void solve2(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 从边缘第一个是o的开始搜索
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    dfs2(board, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs2(char[][] board, int i, int j) {
+        Stack<Pos> stack = new Stack<>();
+        stack.push(new Pos(i, j));
+        board[i][j] = '#';
+        while (!stack.isEmpty()) {
+            // peek the top element of stack, but do not pop out
+            Pos current = stack.peek();
+            // up
+            if (current.i - 1 >= 0
+                    && board[current.i - 1][current.j] == 'O') {
+                stack.push(new Pos(current.i - 1, current.j));
+                board[current.i - 1][current.j] = '#';
+                continue;
+            }
+            // down
+            if (current.i + 1 <= board.length - 1
+                    && board[current.i + 1][current.j] == 'O') {
+                stack.push(new Pos(current.i + 1, current.j));
+                board[current.i + 1][current.j] = '#';
+                continue;
+            }
+            // left
+            if (current.j - 1 >= 0
+                    && board[current.i][current.j - 1] == 'O') {
+                stack.push(new Pos(current.i, current.j - 1));
+                board[current.i][current.j - 1] = '#';
+                continue;
+            }
+            // right
+            if (current.j + 1 <= board[0].length - 1
+                    && board[current.i][current.j + 1] == 'O') {
+                stack.push(new Pos(current.i, current.j + 1));
+                board[current.i][current.j + 1] = '#';
+                continue;
+            }
+            // up there we use continue to go on current round of search
+            // we pop out here to end this search and begin next round
+            stack.pop();
+        }
+    }
+
+    //bfs
+    public void solve3(char[][] board) {
+        if (board == null || board.length == 0) return;
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 从边缘第一个是o的开始搜索
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    bfs(board, i, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    public void bfs(char[][] board, int i, int j) {
+        Queue<Pos> queue = new LinkedList<>();
+        queue.add(new Pos(i, j));
+        board[i][j] = '#';
+        while (!queue.isEmpty()) {
+            Pos current = queue.poll();
+            // up
+            if (current.i - 1 >= 0
+                    && board[current.i - 1][current.j] == 'O') {
+                queue.add(new Pos(current.i - 1, current.j));
+                board[current.i - 1][current.j] = '#';
+                // no continue.
+            }
+            // down
+            if (current.i + 1 <= board.length - 1
+                    && board[current.i + 1][current.j] == 'O') {
+                queue.add(new Pos(current.i + 1, current.j));
+                board[current.i + 1][current.j] = '#';
+            }
+            // left
+            if (current.j - 1 >= 0
+                    && board[current.i][current.j - 1] == 'O') {
+                queue.add(new Pos(current.i, current.j - 1));
+                board[current.i][current.j - 1] = '#';
+            }
+            // right
+            if (current.j + 1 <= board[0].length - 1
+                    && board[current.i][current.j + 1] == 'O') {
+                queue.add(new Pos(current.i, current.j + 1));
+                board[current.i][current.j + 1] = '#';
+            }
+        }
+    }
+
+}
