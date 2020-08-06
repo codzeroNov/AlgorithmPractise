0837959f17454e07a82451532711630905f45fb0
codzeroNov <codzero@qq.com>
2020-08-06 14:47:04 +0800

    Find peak element

---
+++ src/BinarySearch/FindPeakElement.java
@@ -0,0 +1,21 @@
+package BinarySearch;
+
+public class FindPeakElement {
+    /*
+        A peak element is an element that is greater than its neighbors.
+        Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+        The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+        You may imagine that nums[-1] = nums[n] = -∞.
+    */
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
+}
