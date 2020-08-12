18542ddee0e214eeaa2497a25027c1017cb0d928
codzeroNov <codzero@qq.com>
2020-08-12 16:11:39 +0800

    Summary Ranges

---
+++ src/OtherTricky/SummaryRanges.java
@@ -0,0 +1,30 @@
+package OtherTricky;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SummaryRanges {
+    /*
+        Given a sorted integer array without duplicates, return the summary of its ranges.
+    */
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list = new LinkedList<>();
+        int start = 0, end = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                end++;
+                i++;
+            }
+
+            if (start == end) {
+                list.add(String.valueOf(nums[i]));
+            } else {
+                list.add(nums[start] + "->" + nums[end]);
+            }
+            end++;
+            start = end;
+        }
+        return list;
+    }
+
+}
