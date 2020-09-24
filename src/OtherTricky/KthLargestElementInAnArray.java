cbc8d2902bae2a3fe38bbbe95e30a2e604aa89a9
GODzeroNov <codzero@qq.com>
2020-09-24 16:27:31 +0800

    Kth Largest Element In An Array

--- src/OtherTricky/KthLargestElementInAnArray.java
+++ src/OtherTricky/KthLargestElementInAnArray.java
@@ -14,4 +14,47 @@ public class KthLargestElementInAnArray {
         return minHeap.poll();
     }
 
+    public int findKthLargest2(int[] nums, int k) {
+        k = nums.length - k;
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int idx = quickSelect(nums, l, r);
+            if (idx < k) {
+                l = idx + 1;
+            } else if (idx > k) {
+                r = idx - 1;
+            } else {
+                break;
+            }
+        }
+
+        return nums[k];
+    }
+
+    private int quickSelect(int[] nums, int l, int r) {
+        int i = l, j = r, pivot = nums[l];
+
+        while (i < j) {
+            // we select pivot from the very left, so we must start looking the smallest num that bigger than pivot from right.
+            while (i < j && nums[j] >= pivot)
+                j--;
+            while (i < j && nums[i] <= pivot)
+                i++;
+            if (i < j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+        nums[l] = nums[i];
+        nums[i] = pivot;
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        new KthLargestElementInAnArray().findKthLargest2(new int[]{5,2,4,1,3,6,0}, 4);
+    }
+
 }
