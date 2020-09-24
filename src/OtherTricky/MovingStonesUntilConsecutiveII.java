d3fadc94fe9a0437dd8a1b4bdfc1b3c6434494d7
GODzeroNov <codzero@qq.com>
2020-09-24 17:24:07 +0800

    update comment

--- src/OtherTricky/MovingStonesUntilConsecutiveII.java
+++ src/OtherTricky/MovingStonesUntilConsecutiveII.java
@@ -4,16 +4,17 @@ import java.util.Arrays;
 
 public class MovingStonesUntilConsecutiveII {
 /*
-    在一个长度无限的数轴上，第 i 颗石子的位置为 stones[i]。如果一颗石子的位置最小/最大，那么该石子被称作端点石子。
+    在一个长度无限的数轴上，第 i 颗石子的位置为stones[i]。如果一颗石子的位置最小/最大，那么该石子被称作端点石子。
 
     每个回合，你可以将一颗端点石子拿起并移动到一个未占用的位置，使得该石子不再是一颗端点石子。
 
-    值得注意的是，如果石子像 stones = [1,2,5] 这样，你将无法移动位于位置 5 的端点石子，因为无论将它移动到任何位置（例如 0 或 3），该石子都仍然会是端点石子。
+    值得注意的是，如果石子像stones = [1,2,5]这样，你将无法移动位于位置 5 的端点石子，因为无论将它移动到任何位置（例如 0 或 3），该石子都仍然会是端点石子。
 
     当你无法进行任何移动时，即，这些石子的位置连续时，游戏结束。
 
     要使游戏结束，你可以执行的最小和最大移动次数分别是多少？ 以长度为 2 的数组形式返回答案：answer = [minimum_moves, maximum_moves] 。
 */
+    //https://leetcode.com/problems/moving-stones-until-consecutive-ii/discuss/286707/JavaC%2B%2BPython-Sliding-Window
     public int[] numMovesStonesII(int[] stones) {
         int n = stones.length;
         Arrays.sort(stones);
