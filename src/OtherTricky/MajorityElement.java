7459087fd22fc3ab2c3761a56f8bd20596692bcb
GODzeroNov <codzero@qq.com>
2020-10-29 09:25:44 +0800

    Majority Element

---
+++ src/OtherTricky/MajorityElement.java
@@ -0,0 +1,21 @@
+package OtherTricky;
+
+public class MajorityElement {
+    /*
+    Given an array of size n, find the majority element.
+    The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+    You may assume that the array is non-empty and the majority element always exist in the array.
+     */
+
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0], count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) candidate = nums[i];
+
+            count += (nums[i] == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
