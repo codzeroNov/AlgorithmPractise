c316214e037be08a0b8d8e668c922fedc13d5e70
codzeroNov <codzero@qq.com>
2022-05-02 16:08:25 +0800

    update: new method for LinkedListCycleII

--- src/LinkedList/LinkedListCycleII.java
+++ src/LinkedList/LinkedListCycleII.java
@@ -21,4 +21,34 @@ public class LinkedListCycleII {
         return null;
     }
 
+    /**
+     * Let the distance from the first node to the node where cycle begins be A, and let say the slow pointer travels A+B.
+     * The fast pointer must travel 2A+2B to catch up. The cycle size is N.
+     * Full cycle is also how much more fast pointer has traveled than slow pointer at meeting point.
+     *
+     * A+B+N = 2A+2B
+     * N=A+B
+     */
+
+    public ListNode detectCycle2(ListNode head) {
+        if (head == null) return  null;
+
+        ListNode fast = head, slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode slow2 = head;
+                while (slow2 != slow) {
+                    slow = slow.next;
+                    slow2 = slow2.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+
 }
